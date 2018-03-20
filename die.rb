3e377b320f838aab15eeea47a53705a8ef954abe
Nicole-Gasperini <nicole.gasperini88@gmail.com>
2018-03-19 20:59:17 -0600

    First iteration

---
+++ die.rb
@@ -0,0 +1,13 @@
+class Die
+ attr_reader :side_count, :die
+
+  def initialize(side_count = 6, die = 0)
+    @side_count = side_count
+    @die = die
+  end
+
+  def roll
+    # rand(n) will return a number from 0 to one less than n
+    @die = rand(side_count) + 1
+  end
+end
